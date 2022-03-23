d57d1bb92822f033bf32247fc02ee75e451fe162
ivanavi2 <bizbuzbiz@hotmail.com>
2022-03-23 20:34:16 +0800

    Update initial layout
    -remove topbar logo
    -change color theme
    -remove app config floating button

---
+++ src/layouts/PublicLayout/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PublicLayout = () => {
+    return <h1>login</h1>;
+};
+
+export default PublicLayout;
---
+++ src/pages/AddAllowanceClaim.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AddAllowanceClaim = () => {
+    return (
+        <div className="grid">
+            <div className="col-12">
+                <div className="card">
+                    <h5>Empty Page</h5>
+                    <p>Use this page to start from scratch and place your custom content.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const comparisonFn = function (prevProps, nextProps) {
+    return prevProps.location.pathname === nextProps.location.pathname;
+};
+
+export default React.memo(AddAllowanceClaim, comparisonFn);
